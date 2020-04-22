5b2a92ecfbbdec19c6cd41161de8e04e7df17d59
Gonzalo Junco <gonza2609@gmail.com>
2020-04-22 15:32:41 -0500

    primer commit

---
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
@@ -0,0 +1,16 @@
+package academy.digitallab.store.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class ShoppingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingServiceApplication.class, args);
+	}
+
+}
